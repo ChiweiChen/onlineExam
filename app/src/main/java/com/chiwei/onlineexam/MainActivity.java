c9c3717131dda4351e3b0638e28f51007728d3c3
ChiweiChen <chiwei0705@gmail.com>
2020-07-16 10:44:46 +0800

    first commit

---
+++ app/src/main/java/com/chiwei/onlineexam/MainActivity.java
@@ -0,0 +1,26 @@
+package com.chiwei.onlineexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "I love coding";
+                Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
